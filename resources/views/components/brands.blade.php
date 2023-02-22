39716fd0f0c28cbb6591514ed6ef72d8ae4c98eb
nlloydw <nlloydw61@gmail.co.uk>
2023-02-22 10:21:47 +0000

    added more responsive styles to to components

--- resources/views/components/brands.blade.php
+++ resources/views/components/brands.blade.php
@@ -1,12 +1,12 @@
 <section id="brands" class="py-20 mt-1 container mx-auto">
 
-    <h1> {{ $component['label'] }} </h1>
-    <hr class="w-[200px] m-auto bg-[#444] h-[3px] mt-2 mb-4">
-    <p> {{ $component['description'] }} </p>
+    <h1 class="px-4"> {{ $component['label'] }} </h1>
+    <hr class="w-[200px] m-auto bg-[#444] h-[3px] mt-2 mb-4 px-4">
+    <p class="px-4"> {{ $component['description'] }} </p>
 
     <div class="flex flex-col lg:flex-row mt-10 lg:mt-4">
         @foreach($component['brands'] as $brand)
-            <div class="brand lg:w-1/3 px-4 group relative mb-5 lg:mb-0">
+            <div class="brand px-4 group relative mb-5 lg:mb-0">
 
                 <div class="flex flex-col justify-center text-center space-y-5 p-5 lg:hidden" style="background-color: {{ $brand['brand_colour'] }}">
                     <h2>{{ $brand['brand_name'] }}</h2>
@@ -14,11 +14,11 @@
                     <a class="border-2 border-white w-max m-auto py-2.5 px-5" href="{{ $brand['brand_link'] }}">{{ $brand['brand_link_label'] }}</a>
                 </div>
 
-                <img class="w-full" src="{{$brand['brand_image']['permalink']}}" alt="{{ $brand['brand_image']['alt'] }}" width="900" height="1600">
+                <img class="h-full object-cover" src="{{$brand['brand_image']['permalink']}}" alt="{{ $brand['brand_image']['alt'] }}" width="900" height="1600">
 
                 <div class="hidden lg:block absolute z-10 top-0 left-0 w-full h-full px-4">
                     <div class="w-full h-full opacity-0 group-hover:opacity-90 transition-all ease-in-out duration-500" style="background-color: {{ $brand['brand_colour'] }}">
-                        <div class="h-full flex flex-col justify-center text-center space-y-5 px-5">
+                        <div class="h-full flex flex-col justify-center text-center space-y-2 xl:space-y-5 px-5">
                             <h2>{{ $brand['brand_name'] }}</h2>
                             <p>{{ $brand['brand_description'] }}</p>
                             <a class="border-2 border-white w-max m-auto py-2.5 px-5 hover:bg-white hover:text-black transition-all ease-in-out duration-300" href="{{ $brand['brand_link'] }}">{{ $brand['brand_link_label'] }}</a>
--- resources/views/components/cta_banner.blade.php
+++ resources/views/components/cta_banner.blade.php
@@ -1,4 +1,4 @@
-<section id="cta_banner" class="flex flex-col md:flex-row justify-center bg-primary py-2 px-2 md:p-0">
+<section id="cta_banner" class="flex flex-col md:flex-row justify-center bg-primary py-2 px-4 md:p-0">
 
     <h1 class="md:pr-4 text-center"> {{ $component['label'] }} </h1>
     <a class="px-4 mx-auto md:mx-0 my-auto w-max border-2 border-white hover:bg-white hover:text-black transition-all ease-in-out duration-300" href="{{ $component['link'] }}">{{ $component['button'] }}</a>
