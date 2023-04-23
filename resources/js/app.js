a5770dbc89533c2c785cd9b2cc5c121522c87286
nlloydw <nlloydw61@gmail.co.uk>
2023-04-23 14:30:23 +0100

    forgot to push up app.js file

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,15 @@
-import './scripts/navigation_menu';
-import './scripts/contact_form';
+require('./bootstrap')
 
+import { createApp } from 'vue'
+
+import contact_us from "./components/contact_us.vue";
+
+import '../css/app.css';
+
+import './scripts/navigation_menu';
+
+const app = createApp({})
+
+app.component('contact-us', contact_us)
+
+app.mount('#app')
